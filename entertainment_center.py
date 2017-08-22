50506764a3ca1a79bed49cd4dcc8300b906138bd
Jesse-Chang <sowhat1206@gmail.com>
2017-08-22 10:06:18 +0800

    Update entertainment_center.py
    modify variable names to lowercase

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,7 +2,7 @@ import media
 import fresh_tomatoes
 
 # Create instance for each movie
-Harry_Potter_01 = media.Movie("Harry Potter and the Sorcerer's Stone",
+harry_potter_01 = media.Movie("Harry Potter and the Sorcerer's Stone",
                               "Rescued from the outrageous neglect of his"
                               " aunt and uncle, a young boy with a great"
                               " destiny proves his worth while attending"
@@ -10,7 +10,7 @@ Harry_Potter_01 = media.Movie("Harry Potter and the Sorcerer's Stone",
                               "https://tinyurl.com/yb2os4mk",
                               "https://www.youtube.com/watch?v=z86aFALzjgs")
 
-Harry_Potter_02 = media.Movie("Harry Potter and the Chamber of Secrets",
+harry_potter_02 = media.Movie("Harry Potter and the Chamber of Secrets",
                               "Harry ignores warnings not to return to"
                               " Hogwarts, only to find the school plagued"
                               " by a series of mysterious"
@@ -18,7 +18,7 @@ Harry_Potter_02 = media.Movie("Harry Potter and the Chamber of Secrets",
                               "https://tinyurl.com/yddd7pkk",
                               "https://www.youtube.com/watch?v=GQAxGb5njBk")
 
-Harry_Potter_03 = media.Movie("Harry Potter and the Prisoner of Azkaban",
+harry_potter_03 = media.Movie("Harry Potter and the Prisoner of Azkaban",
                               "It's Harry's third year at Hogwarts;"
                               " not only does he have a new Defense"
                               " Against the Dark Arts teacher, but"
@@ -29,7 +29,7 @@ Harry_Potter_03 = media.Movie("Harry Potter and the Prisoner of Azkaban",
                               "https://tinyurl.com/ybf6nssx",
                               "https://www.youtube.com/watch?v=-S79A0gIZSE")
 
-Harry_Potter_04 = media.Movie("Harry Potter and the Goblet of Fire",
+harry_potter_04 = media.Movie("Harry Potter and the Goblet of Fire",
                               "A young wizard finds himself competing in a "
                               "hazardous tournament"
                               " between rival schools of magic, but he is"
@@ -37,7 +37,7 @@ Harry_Potter_04 = media.Movie("Harry Potter and the Goblet of Fire",
                               "https://tinyurl.com/y8cdfn44",
                               "https://www.youtube.com/watch?v=_i0EN_YHdYA")
 
-Harry_Potter_05 = media.Movie("Harry Potter and the Order of the Phoenix",
+harry_potter_05 = media.Movie("Harry Potter and the Order of the Phoenix",
                               "With their warning about Lord Voldemort's"
                               " return scoffed at, Harry and Dumbledore"
                               " are targeted by the Wizard authorities"
@@ -46,7 +46,7 @@ Harry_Potter_05 = media.Movie("Harry Potter and the Order of the Phoenix",
                               "https://tinyurl.com/y6wl9cjc",
                               "https://www.youtube.com/watch?v=Niave1bUkHw")
 
-Harry_Potter_06 = media.Movie("Harry Potter and the Half-Blood Prince",
+harry_potter_06 = media.Movie("Harry Potter and the Half-Blood Prince",
                               "As Harry Potter begins his sixth year at"
                               " Hogwarts, he discovers an old"
                               " book marked as the property of the"
@@ -56,7 +56,7 @@ Harry_Potter_06 = media.Movie("Harry Potter and the Half-Blood Prince",
                               "https://tinyurl.com/y9fyem4e",
                               "https://www.youtube.com/watch?v=0gV8CdBxmjI")
 
-Harry_Potter_07 = media.Movie("Harry Potter and the Deathly Hallows: Part 1",
+harry_potter_07 = media.Movie("Harry Potter and the Deathly Hallows: Part 1",
                               "As Harry races against time and evil to "
                               "destroy the Horcruxes, he uncovers the"
                               " existence of three"
@@ -65,7 +65,7 @@ Harry_Potter_07 = media.Movie("Harry Potter and the Deathly Hallows: Part 1",
                               "https://tinyurl.com/ybjl9559",
                               "https://www.youtube.com/watch?v=l4syK6Nwdcw")
 
-Harry_Potter_08 = media.Movie("Harry Potter and the Deathly Hallows: Part 2",
+harry_potter_08 = media.Movie("Harry Potter and the Deathly Hallows: Part 2",
                               "Harry, Ron and Hermione search for Voldemort's"
                               " remaining Horcruxes"
                               " in their effort to destroy the Dark Lord"
@@ -73,7 +73,7 @@ Harry_Potter_08 = media.Movie("Harry Potter and the Deathly Hallows: Part 2",
                               "https://tinyurl.com/ybn5z2rp",
                               "https://www.youtube.com/watch?v=jgpJoKDzoZ4")
 # set of movie to creat page
-movies = [Harry_Potter_01, Harry_Potter_02, Harry_Potter_03, Harry_Potter_04,
-          Harry_Potter_05, Harry_Potter_06, Harry_Potter_07, Harry_Potter_08]
+movies = [harry_potter_01, harry_potter_02, harry_potter_03, harry_potter_04,
+          harry_potter_05, harry_potter_06, harry_potter_07, harry_potter_08]
 
 fresh_tomatoes.open_movies_page(movies)
